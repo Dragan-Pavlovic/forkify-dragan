4ea91b4faa72fa352ef9974898922ae9647c51cc
Dragan-Pavlovic <obuca.kalista@gmail.com>
2021-10-06 18:38:46 +0200

    Turned LOGing OFF

--- src/js/config.js
+++ src/js/config.js
@@ -3,9 +3,9 @@ export const API_KEY = '952b7499-c262-4a0b-b100-db928ea2a66e';
 export const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
 
 export const TIMEOUT_SEC = 5;
-export const LOG_ERROR_MESSAGE = true;
+export const LOG_ERROR_MESSAGE = false;
 export const LOG_ERROR = false;
 export const LOG_ERROR_ALL = false;
-export const LOG_STATE = true;
+export const LOG_STATE = false;
 export const RESULTS_PER_PAGE = 10;
 export const SUCCESS_TIMEOUT_MS = 1500;
